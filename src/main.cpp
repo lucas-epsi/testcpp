0458fcf7e4132a8ae6bfdec9f8db8706ebe81b73
lucas <lucas.forentin1@epsi.fr>
2018-09-25 12:56:42 -0500

    last release, everything should be ok

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,8 @@ calcul first;
 ecole secund;
 
 std::cout<<first.somme(63,898)<<std::endl;
-std::cout<<secund.date()<<std::endl;
+std::cout<< "voici la date du jour:  "<<secund.date()<<std::endl;
 
-string secundate = secund.date();
-
-cout << secund.date();
-cout <<"yo voici la date :"<< secundate ;
 
 return 0;
 
