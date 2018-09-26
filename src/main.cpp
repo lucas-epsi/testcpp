779e05d9a6a4f961831c69db311bba336a46e993
lucas <lucas.forentin1@epsi.fr>
2018-09-26 02:43:24 -0500

    ajout d'une instanciation dynamique en pointeurs

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,11 @@ calcul multiply;
 calcul divide;
 ecole secund;
 
+calcul* calculpointeur = new calcul();
+
+cout << calculpointeur -> multiplication(903,986) <<endl; 
+
+
 cout<<multiply.multiplication(53,60)<<endl;
 cout<<divide.division(83,2)<<endl;
 std::cout<<first.somme(63,898)<<std::endl;
